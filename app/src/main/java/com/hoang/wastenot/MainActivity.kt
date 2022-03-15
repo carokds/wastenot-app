67b2a2701c103b1ff79341438410d640cd68cc64
aliciarodriguez <alimatama@gmail.com>
2022-03-15 18:38:10 +0100

    Koin bug solved: moved startKoin from MainActivity to KoinApplication

--- app/src/main/java/com/hoang/wastenot/MainActivity.kt
+++ app/src/main/java/com/hoang/wastenot/MainActivity.kt
@@ -2,8 +2,6 @@ package com.hoang.wastenot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import com.hoang.wastenot.databinding.ActivityMainBinding
 import com.hoang.wastenot.di.KoinGraph
 import org.koin.android.ext.koin.androidContext
@@ -13,11 +11,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        startKoin {
-            androidContext(applicationContext)
-            modules(KoinGraph.mainModules)
-        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
--- app/src/main/java/com/hoang/wastenot/fragments/RecipesFragment.kt
+++ app/src/main/java/com/hoang/wastenot/fragments/RecipesFragment.kt
@@ -2,9 +2,9 @@ package com.hoang.wastenot.fragments
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hoang.wastenot.R
 import com.hoang.wastenot.adapters.RecipeAdapter
