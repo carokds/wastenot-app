c52fdf9b66a12eebe38e672d153ace6c8489605e
veronica <berovikaki@gmail.com>
2022-03-16 15:38:47 +0100

    seems to be working?!?!?!

--- app/src/main/java/com/hoang/wastenot/Notifications.kt
+++ app/src/main/java/com/hoang/wastenot/Notifications.kt
@@ -2,6 +2,7 @@ package com.hoang.wastenot
 
 
 import android.R
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -16,50 +17,51 @@ import com.hoang.wastenot.databinding.NotificationsFragmentBinding
 
 class Notifications : AppCompatActivity() {
 
+    private val CHANNEL_ID = "EXPIRY"
+
     private lateinit var binding: NotificationsFragmentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = NotificationsFragmentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.stat_notify_chat)
+            .setContentTitle("Your ingredient is expiring soon!")
+            .setContentText("Check suggested recipes")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setCategory(NotificationCompat.CATEGORY_REMINDER)
+            .setAutoCancel(true)
     }
 
-    val intent = Intent(this, AlertDetails::class.java).apply {
-        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-    }
+
 
     private val pendingIntent: PendingIntent =
         PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
-
-    var builder = NotificationCompat.Builder(this, CHANNEL_ID)
-        .setSmallIcon(R.drawable.stat_notify_chat)
-        .setContentTitle("Your ingredient is expiring soon!")
-        .setContentText("Check suggested recipes")
-        .setPriority(NotificationCompat.PRIORITY_HIGH)
-        .setContentIntent(pendingIntent)
-        .setCategory(NotificationCompat.CATEGORY_REMINDER)
-        .setAutoCancel(true)
-
     private fun createNotificationChannel() {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = getString(R.string.channel_name)
-            val descriptionText = getString(R.string.channel_description)
+            val name = "Expiry Notification"
+            val descriptionText = "Receive reminders for expiring goods"
             val importance = NotificationManager.IMPORTANCE_DEFAULT
             val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
                 description = descriptionText
             }
-            // Register the channel with the system
+
             val notificationManager: NotificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
+
+            }
         }
 
 
     }
 
-}
+
 
 
 
