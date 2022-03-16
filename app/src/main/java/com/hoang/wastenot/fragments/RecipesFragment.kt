929c1c60c0ac6eb6e3c3bc6f675386d41b7b192f
alimatama <91776278+alimatama@users.noreply.github.com>
2022-03-16 13:16:53 +0100

    Revert "Recipe api"

--- app/src/main/java/com/hoang/wastenot/fragments/RecipesFragment.kt
+++ app/src/main/java/com/hoang/wastenot/fragments/RecipesFragment.kt
@@ -2,48 +2,26 @@ package com.hoang.wastenot.fragments
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.navigation.Navigation
 import com.hoang.wastenot.R
-import com.hoang.wastenot.adapters.RecipeAdapter
 import com.hoang.wastenot.databinding.FragmentRecipesBinding
-import com.hoang.wastenot.viewmodels.RecipeViewModel
-
 
 class RecipesFragment : Fragment(R.layout.fragment_recipes) {
 
     private lateinit var binding: FragmentRecipesBinding
-    private val viewModel by viewModels<RecipeViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentRecipesBinding.bind(view)
-viewModel.getRecipes()
 
-        with(binding.recyclerView) {
-            layoutManager = LinearLayoutManager(context)
-            adapter = RecipeAdapter()
-            }
-
-        viewModel.recipes.observe(viewLifecycleOwner) {
-            (binding.recyclerView.adapter as RecipeAdapter).dataSet = it
-//            Toast.makeText(context, "${it[0].title}",Toast. LENGTH_LONG ).show()
-
+        binding.ivTemporaryRecipes.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_recipesFragment_to_recipeDetailFragment)
         }
-
-
-
-//        binding.
-//            .setOnClickListener {
-//            Navigation.findNavController(view).navigate(R.id.action_recipesFragment_to_recipeDetailFragment)
-//        }
 
         binding.btnHomeRecipesfragment.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_global_inventoryFragment)
         }
-
     }
 }
