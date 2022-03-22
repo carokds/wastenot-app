d4ec8a7542e03ffa23010e28ae95393bc1ad193f
helenhoang <helenhoang47@gmail.com>
2022-03-22 13:26:46 +0100

    buttons replacing scroll

--- app/src/main/java/com/hoang/wastenot/fragments/RecipesFragment.kt
+++ app/src/main/java/com/hoang/wastenot/fragments/RecipesFragment.kt
@@ -65,38 +65,60 @@ class RecipesFragment : Fragment(R.layout.fragment_recipes) {
             (binding.recyclerView.adapter as RecipeAdapter).dataSet = it.results
         }
 
-        val diets: Array<out String> = resources.getStringArray(R.array.diet_array)
-        val textView = (binding.autocompleteDiet as AutoCompleteTextView)
-        ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, diets).also { adapter ->
-            textView.setAdapter(adapter)
-        }
-
-       textView.setText(diets[0],false)
-        // Get input text
-        val inputText = textView?.text.toString()
-
-
-        textView?.doOnTextChanged { inputText, _, _, _ ->
-            Toast.makeText(context,inputText, Toast.LENGTH_SHORT).show()
-            binding.menu.error = null
-
+//        val diets: Array<out String> = resources.getStringArray(R.array.diet_array)
+//        val textView = (binding.autocompleteDiet as AutoCompleteTextView)
+//        ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, diets).also { adapter ->
+//            textView.setAdapter(adapter)
+//        }
+//
+//       textView.setText(diets[0],false)
+//        // Get input text
+//        val inputText = textView?.text.toString()
+//
+//
+//        textView?.doOnTextChanged { inputText, _, _, _ ->
+//            Toast.makeText(context,inputText, Toast.LENGTH_SHORT).show()
+//            binding.menu.error = null
+//
             this.arguments?.getParcelable<Food>("Food").apply {
-                when (inputText.toString()) {
-                    diets[1] -> this?.let { viewModel.getRecipes(it.category, "gluten free") }
-                    diets[2] -> this?.let { viewModel.getRecipes(it.category, "ketogenic") }
-                    diets[3] -> this?.let { viewModel.getRecipes(it.category, "vegetarian") }
-                    diets[4] -> this?.let { viewModel.getRecipes(it.category, "vegan") }
-                    diets[5] -> this?.let { viewModel.getRecipes(it.category, "pescetarian") }
-                    diets[6] -> this?.let { viewModel.getRecipes(it.category, "low fodmap") }
-                    diets[0] -> this?.let { viewModel.getRecipes(it.category, " ") }
-                    else -> {
-                        binding.menu.error = "error"
-                    }
-
+                binding.btnDietNone.setOnClickListener {
+                    this?.let { viewModel.getRecipes(it.category, "") }
 
                 }
+                binding.btnDietVegan.setOnClickListener {
+                    this?.let { viewModel.getRecipes(it.category, "vegan") }
+                }
+                binding.btnDietVegetarian.setOnClickListener {
+                    this?.let { viewModel.getRecipes(it.category, "vegetarian") }
+                }
+                binding.btnDietGluten.setOnClickListener {
+                    this?.let { viewModel.getRecipes(it.category, "gluten free") }
+                }
+                binding.btnDietPescatarian.setOnClickListener {
+                    this?.let { viewModel.getRecipes(it.category, "pescetarian") }
+                }
+                binding.btnDietKeto.setOnClickListener {
+                    this?.let { viewModel.getRecipes(it.category, "ketogenic") }
+
+                }
+                binding.btnDietFodmap.setOnClickListener {
+                    this?.let { viewModel.getRecipes(it.category, "low fodmap") }
+                }
+//                    diets[1] -> this?.let { viewModel.getRecipes(it.category, "gluten free") }
+//                    diets[2] -> this?.let { viewModel.getRecipes(it.category, "ketogenic") }
+//                    diets[3] -> this?.let { viewModel.getRecipes(it.category, "vegetarian") }
+//                    diets[4] -> this?.let { viewModel.getRecipes(it.category, "vegan") }
+//                    diets[5] -> this?.let { viewModel.getRecipes(it.category, "pescetarian") }
+//                    diets[6] -> this?.let { viewModel.getRecipes(it.category, "low fodmap") }
+//                    diets[0] -> this?.let { viewModel.getRecipes(it.category, " ") }
+//                    else -> {
+//                        binding.menu.error = "error"
+//                    }
+
+
+
             }
-     }
+//     }
 
 //        binding.
 //            .setOnClickListener {
