c01df0058d7f15c53111692a0c4743a02f161bc0
helenhoang <helenhoang47@gmail.com>
2022-03-24 15:51:14 +0100

    api key hidden

--- app/src/main/java/com/hoang/wastenot/repositories/RecipeRepository.kt
+++ app/src/main/java/com/hoang/wastenot/repositories/RecipeRepository.kt
@@ -2,16 +2,19 @@ package com.hoang.wastenot.repositories
 
 import android.util.Log
 import androidx.lifecycle.LiveData
+import com.hoang.wastenot.BuildConfig
 import com.hoang.wastenot.api.RecipeDetailResponse
 import com.hoang.wastenot.api.RecipeResponse
 import com.hoang.wastenot.api.RecipeRootResponse
 import com.hoang.wastenot.api.Retrofit.recipeService
+
 import kotlinx.coroutines.flow.Flow
 
 
 class RecipeRepository {
 
-val API_KEY = "bce7f13a45eb414bb1956295ee6b859d"
+    val API_KEY = BuildConfig.SPOONACULAR_KEY
+
 
     suspend fun getRecipes(ingredient: String, diet: String): RecipeRootResponse? {
         val response =
