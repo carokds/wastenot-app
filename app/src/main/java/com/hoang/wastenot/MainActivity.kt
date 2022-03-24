76b49d03e5f7d797c77a2c58ce617516198f69fb
Carolina Kakefuku <carolinakakefuku@gmail.com>
2022-03-24 23:22:11 +0100

    InputMethodManager now being called from AddFragment

--- app/src/main/java/com/hoang/wastenot/MainActivity.kt
+++ app/src/main/java/com/hoang/wastenot/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.hoang.wastenot
 
-import android.content.Context
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.hoang.wastenot.databinding.ActivityMainBinding
 
@@ -15,12 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
-        return true
     }
 
 }
